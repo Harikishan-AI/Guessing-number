0d75b848fe6e8dc382f51060e81c37da9a5a0352
harikishan <harikishantakoor@gmail.com>
2021-09-04 09:50:26 +0530

    second commit

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-print("Guessing number\n")
+print("---------Guessing number----------")
+print("--------9 Chances are given-------\n")
 a = 18
 no_of_guess = 9
 count = 0
 while True:
-    user = int(input("number: "))
+    user = int(input("Please enter the number: "))
     count = count + 1
     no_of_guess = no_of_guess - 1
     if user < a and count < 10:
-        print("Guessing number is lesser")
-        print("Number of guesses left out ", no_of_guess)
+        print("Guessing number is lesser please enter greater number")
+        print("Number of guesses left out --> ", no_of_guess)
         if no_of_guess == 0:
             print("Game over!!!")
             break
     elif user > a and count < 10:
-        print("Guessing number is greater")
-        print("Number of guesses left out ", no_of_guess)
+        print("Guessing number is greater please enter lesser number")
+        print("Number of guesses left out --> ", no_of_guess)
         if no_of_guess == 0:
             print("Game Over!!!")
             break
     elif user == a and count < 10:
-        print("You won!!!")
-        print("Number of chances taken", count)
-        print("Number of Guesses left out ", no_of_guess)
+        print("-------You won!!!-------")
+        print("Number of chances taken including now --> ", count)
+        print("Number of Guesses left out --> ", no_of_guess)
         break
